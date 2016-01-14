ec5f3f9eb8a569b2ad4da5a58e92f5bdabaed5b6
unknown <a.bolt@tue.nl>
2016-01-14 15:59:17 +0100

    log import works, now working on the synchronizer

--- src/application/controllers/importdata/ImportDataController.java
+++ src/application/controllers/importdata/ImportDataController.java
@@ -2,8 +2,15 @@ package application.controllers.importdata;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Collection;
 import java.util.ResourceBundle;
 
+import org.deckfour.xes.in.XUniversalParser;
+import org.deckfour.xes.model.XLog;
+
+import application.controllers.AbstractController;
+import application.controllers.synchronization.Synchronizer;
+import application.models.eventlog.EventLog;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -16,8 +23,9 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
-public class ImportDataController implements Initializable {
+public class ImportDataController extends AbstractController implements Initializable {
 
+	
 	@FXML
 	private ScrollPane importDataPanel;
 
@@ -38,7 +46,8 @@ public class ImportDataController implements Initializable {
 	protected void openFileSelectorButton(ActionEvent event) {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
-		fileChooser.getExtensionFilters().add(new ExtensionFilter("XES or CSV files supported", "*.xes", "*.csv"));
+		fileChooser.getExtensionFilters()
+				.add(new ExtensionFilter("XES or CSV files supported", "*.xes", "*.xes.gz", "*.csv"));
 		File selectedFile = fileChooser.showOpenDialog(null);
 		if (selectedFile != null) {
 			fileName.setText(selectedFile.getPath());
@@ -49,6 +58,21 @@ public class ImportDataController implements Initializable {
 	protected void handleImportDataButton(ActionEvent event) {
 		if (checkSeparators()) {
 			// here i do the importing.
+			try {
+				XLog log = null;
+				XUniversalParser parser = new XUniversalParser();
+				File file = new File(fileName.getText());
+				if (parser.canParse(file)) {
+					Collection<XLog> collection = parser.parse(file);
+					log = !collection.isEmpty() ? collection.iterator().next() : null;
+
+					EventLog.setXLog(log);
+					setStable();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				logErrorMessage();
+			}
 		}
 
 	}
@@ -76,6 +100,15 @@ public class ImportDataController implements Initializable {
 		alert.setHeaderText("Invalid Separator(s)");
 		alert.setContentText("Please check that the field and value separators are valid: "
 				+ "they cannot not be equal (the field separator is mandatory)");
+		alert.showAndWait();
+	}
+
+	protected void logErrorMessage() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText("Invalid Event Log");
+		alert.setContentText(
+				"The event log that you are trying to impor is not valid. Please check that you are using the right one!");
 		alert.showAndWait();
 	}
 
--- src/application/controllers/synchronization/Synchronizer.java
+++ src/application/controllers/synchronization/Synchronizer.java
@@ -1,5 +1,51 @@
 package application.controllers.synchronization;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import application.controllers.AbstractController;
+
+/**
+ * Synchronizes the different steps
+ * 
+ * @author abolt
+ *
+ */
+
 public class Synchronizer {
+
+	/**
+	 * @author abolt
+	 * 
+	 *         If a controller triggers, all future steps are unstabilized.
+	 *
+	 */
+
+	private static List<AbstractController> controllerList = new Vector<AbstractController>();
+
+	public static void addController(AbstractController newController) {
+
+		if (controllerList.isEmpty())
+			controllerList.add(newController);
+
+		else {
+			boolean added = false;
+			for (AbstractController controller : controllerList) {
+				if (added)
+					controller.setUnstable();
+				if (newController.getClass().isInstance(controller)) {
+					added = true;
+					controller = newController;
+				}
+			}
+			if (!added)
+				controllerList.add(newController);
+		}
+	}
+
+	public static void synchronize(AbstractController trigger) {
+
+	}
 
 }
